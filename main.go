cc15e4cad0ec529787331e9bc57d1b6a45298976
BubbleFish <daforsastudia@gmail.com>
2025-04-12 21:31:30 +0300

    refactor: Remove unused landing card HTMX endpoint

--- main.go
+++ main.go
@@ -18,9 +18,6 @@ func main() {
 
 	http.HandleFunc("/", server.Index)
 
-	// http.HandleFunc("/htmx/landing_card/{sort}", server.HTMXLandingCard)
-	// http.HandleFunc("/htmx/landing/get_newest_card", pages.HTMXCardPack("newest"))
-
 	server.SetupLogger(os.Getenv("DEBUG") == "1")
 	err := http.ListenAndServe("127.0.0.1:8080", nil)
 	if err != nil {

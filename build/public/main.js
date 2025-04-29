b806ee12993087218cc2c3c9341f98985b14ff68
BubbleFish <daforsastudia@gmail.com>
2025-04-29 13:36:09 +0300

    feat: Make <select> close when clicked outside

--- build/public/main.js
+++ build/public/main.js
@@ -1,3 +1,14 @@
+self.addEventListener(
+  "click",
+  /** @param {MouseEvent} event */
+  (event) => {
+    for (const widget of self.WIDGETS) {
+      if (!widget.node.contains(event.target)) {
+        widget.close();
+      }
+    }
+  },
+);
 class SelectWidget {
   /** @type {HTMLDivElement} */
   node;

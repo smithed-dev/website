5bdbe1e1790259ed6951ab34e89cdf9fa9297c15
BubbleFish <daforsastudia@gmail.com>
2025-05-06 17:38:50 +0300

    feat: Don't specify the local ip

--- go.mod
+++ go.mod
@@ -20,5 +20,5 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	http.HandleFunc("/htmx/browse_packs", server.HTMXBrowsePacks)
 
 	server.SetupLogger(os.Getenv("DEBUG") == "1")
-	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}

2aaad32ca1ec1b5bf4aac92a36c68952160d4b89
BubbleFish <daforsastudia@gmail.com>
2025-05-06 15:58:41 +0300

    feat: Make user dropdown work

---
+++ web/pages/components/root.js
@@ -0,0 +1,31 @@
+class LoginWidget {
+  /** @type {HTMLButtonElement} */
+  node;
+
+  constructor(node) {
+    this.node = node;
+    ClosableWidgets.push(this);
+  }
+
+  close() {
+    document
+      .getElementById("profile-dropdown")
+      .style.removeProperty("visibility");
+  }
+}
+
+/** @param {HTMLDivElement} node */
+function gotoSearchPage(node) {
+  const uri = new URL(self.location.origin);
+  uri.searchParams.set("search", node.querySelector("input")?.value);
+
+  self.location.href = encodeURI(
+    [self.location.origin, "/browse?" + uri.searchParams.toString()].join("/"),
+  );
+}
+
+/** @param {HTMLButtonElement} node */
+function toggleDropdown() {
+  const dropdown = document.getElementById("profile-dropdown");
+  dropdown.style.setProperty("visibility", "visible");
+}

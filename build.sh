1229dd9e5f3612696377ea705298671b24fc8fc9
BubbleFish <daforsastudia@gmail.com>
2025-05-15 22:21:50 +0300

    feat: Make build.sh only build HTML and ensure Mend is installed

--- build.sh
+++ build.sh
@@ -1,22 +1,60 @@
 #!/usr/bin/bash
-# This is a helper script to run all the scripts
 
-function do_css {
-    echo "==> CSS: Started"
-    ./generate.css.sh
-    echo "==> CSS: Done"
+MEND_VERSION=v1.0.1-alpha.4
+
+function install_mend {
+    echo "==> Mend: Installing the required version" >&2
+    go install github.com/bbfh-dev/mend@$MEND_VERSION
 }
 
-function do_js {
-    echo "==> JS: Started"
-    ./generate.js.sh
-    echo "==> JS: Done"
+if ! [ -x "$(command -v mend)" ]; then
+    install_mend
+fi
+
+if mend --version | grep -q $MEND_VERSION; then
+    echo "==> Mend: Installation is correct"
+else
+    install_mend
+fi
+
+echo "==> Mend: Started"
+mkdir -p ./build/
+mkdir -p ./build/htmx/
+
+cp -fr ./web/public/ ./build/
+
+CSS_CHECKSUM=$(md5sum ./build/public/generated/styles.min.css | cut -d ' ' -f1)
+JS_CHECKSUM=$(md5sum ./build/public/generated/main.js | cut -d ' ' -f1)
+
+function mend_wrapper {
+    file=$1
+    dir=$2
+    base=$(basename $file)
+    js="./build/public/page/$(basename -s .html $file).js"
+
+    PAGE_CHECKSUM=""
+    if [[ -f $js ]]; then
+        PAGE_CHECKSUM=$(md5sum $js | cut -d ' ' -f1)
+    fi
+
+    mend --work-dir ./web/pages/ \
+        --input checksum.css=$CSS_CHECKSUM,checksum.js=$JS_CHECKSUM,checksum.page_js=$PAGE_CHECKSUM \
+        --output $dir/. $file \
+        2> >(sed $'s/^/\e[31m/; s/$/\e[0m/' >&2) \
+        && echo "--- Built $dir/$base"
 }
 
-do_css &
-do_js &
+for file in ./web/pages/*.html; do
+    if [[ -f $file ]]; then
+        mend_wrapper $file ./build &
+    fi
+done
+
+for file in ./web/pages/htmx/*.html; do
+    if [[ -f $file ]]; then
+        mend_wrapper $file ./build/htmx &
+    fi
+done
+
 wait
-
-echo "==> HTML: Started"
-./generate.html.sh
-echo "==> HTML: Done"
+echo "==> Mend: Done"
